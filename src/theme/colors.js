e53083cbe38bc0a0eaedfd3aa709ae99896faef8
ronmnm <mona@lisa.com>
2020-05-14 11:21:43 +0300

    init commit

---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+   white: '#ffffff',
+   black: '',
+   grey: '',
+   yellowPrimary: '',
+   yellowHover: '',
+   yellowOnClick: '',
+   textBlack: '#282828',
+   textGrey: '',
+   textLightGrey: '',
+};
