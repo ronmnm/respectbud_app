d0fb1fa9216812cefe32c531748a7029f6961b39
ronmnm <spellbound.2808@gmail.com>
2020-05-28 14:40:50 +0300

    googel maps + input autocomplete dropdown

--- src/components/desktop/main-content/main-section/white-section/calculate-form.jsx
+++ src/components/desktop/main-content/main-section/white-section/calculate-form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { PrimaryButton } from '../../../../elements/buttons';
 import { InputStyled } from '../../../../elements/input';
@@ -7,142 +7,146 @@ import { connect } from 'react-redux';
 import * as t from '../../../../../redux/actionTypes';
 import { Dropdown } from '../../../../elements/dropdown';
 import { registerNewCustomer } from '../../../../../services/register';
+import { getPrice } from '../../../../../services/get-price';
+import InputPlaces from './places-input';
 
 const CalculateFormStyled = styled.div`
-   width: 100%;
-   display: grid;
-   /* grid-template-rows: min-content min-content; */
-   align-items: center;
-   .necessary_field{
-      font-size: 13px;
-      font-weight: 400;
-      color: ${({theme}) => theme.textGrey};
-      margin-left: 30px;
-   }
-   .calculate_form_wrapper {
+  width: 100%;
+  display: grid;
+  /* grid-template-rows: min-content min-content; */
+  align-items: center;
+  .necessary_field {
+    font-size: 13px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.textGrey};
+    margin-left: 30px;
+  }
+  .calculate_form_wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 280px;
+    column-gap: 20px;
+    padding-left: 20px;
+    margin-bottom: 20px;
+    .calculate_form_column {
       display: grid;
-      grid-template-columns: 1fr 1fr;
-      height: 280px;
-      column-gap: 20px;
-      padding-left: 20px;
-      margin-bottom: 20px;
-      .calculate_form_column {
-         display: grid;
-         align-items: flex-end;
-         grid-template-rows: repeat(3, 1fr);
-         .input_label {
-            display: inline-block;
-            margin-bottom: 5px;
-            margin-left: 15px;
-            font-size: 14px;
-            font-weight: 400;
-            color: ${({theme}) => theme.textGrey};
-         }
+      align-items: flex-end;
+      grid-template-rows: repeat(3, 1fr);
+      .input_label {
+        display: inline-block;
+        margin-bottom: 5px;
+        margin-left: 15px;
+        font-size: 14px;
+        font-weight: 400;
+        color: ${({ theme }) => theme.textGrey};
       }
-   }
+    }
+  }
 `;
 
 const CalculateForm = ({
-   dispatch,
-   customerName,
-   customerOrganization,
-   customerPhone,
-   customerPaymentMethod,
-   customerAddress,
-   materialWeight,
-   materialVolume,
+  dispatch,
+  customerName,
+  customerOrganization,
+  customerPhone,
+  customerPaymentMethod,
+  addr,
+  weight,
+  paymentMethodList,
+  materialTypeTitle,
+  paymentMethodAlias,
+  selectedCoordinates,
+  phone,
 }) => {
-   function checkButtonIfDisable() {
-      if (
-         !customerName ||
-         customerPhone.length !== 19 ||
-         !customerPaymentMethod ||
-         !customerAddress ||
-         !materialWeight ||
-         !materialVolume
-      ) {
-         return true;
-      }
-      return false;
-   }
-   const paymentMethodList = [
-      { title: 'Наличный расчет', id: 0 },
-      { title: 'Безналичный расчет', id: 1 },
-   ];
+  const [isLoading, setIsLoading] = useState(0)
 
-   function handleCalculateClick() {
-      registerNewCustomer(customerName, customerPhone, customerOrganization)
+  async function handleCalculateClick() {
+    await registerNewCustomer(customerName, customerPhone, customerOrganization)
 
-      dispatch({ type: t.SET_CURRENT_COMPONENT, payload: t.RESULT_PAGE })
-   }
+    setIsLoading(1)
+    const result = await getPrice(addr, materialTypeTitle, weight, paymentMethodAlias, selectedCoordinates, phone);
+    console.log(result);
+    dispatch({type: t.SET_FINAL_PRICE, payload: Math.ceil(Math.min(...result.finalPrices) / 10) * 10})
+    setIsLoading(0)
 
-   return (
-      <CalculateFormStyled>
-         <div>
-            <div className="calculate_form_wrapper">
-               <div className="calculate_form_column">
-                  <div>
-                     <span className="input_label">* Ваше имя</span>
-                     <InputStyled
-                        border
-                        placeholder="Петро"
-                        value={customerName || ''}
-                        onChange={e => dispatch({ type: t.SET_CUSTOMER_NAME, payload: e.target.value })}
-                     />
-                  </div>
-                  <div>
-                     <span className="input_label">Название организации</span>
-                     <InputStyled
-                        border
-                        placeholder="ТОВ"
-                        value={customerOrganization || ''}
-                        onChange={e => dispatch({ type: t.SET_CUSTOMER_ORGANIZATION, payload: e.target.value })}
-                     />
-                  </div>
-                  <div>
-                     <span className="input_label">* Номер телефона</span>
-                     <PhoneInput
-                        value={customerPhone || ''}
-                        onChange={value => dispatch({ type: t.SET_CUSTOMER_PHONE, payload: value })}
-                     />
-                  </div>
-               </div>
-               <div className="calculate_form_column">
-                  <div>
-                     <span className="input_label">* Форма оплаты</span>
-                     <Dropdown
-                        selectedItem={customerPaymentMethod}
-                        callback={(title, id) => dispatch({ type: t.SET_CUSTOMER_PAYMENT_METHOD, payload: title })}
-                        withBorder={true}
-                        list={paymentMethodList}
-                     />
-                  </div>
-                  <div>
-                     <span className="input_label">* Адрес</span>
-                     <InputStyled
-                        border
-                        placeholder="г. Киев, ул. Олеся Гончара,2"
-                        value={customerAddress || ''}
-                        onChange={e => dispatch({ type: t.SET_CUSTOMER_ADDRESS, payload: e.target.value })}
-                     />
-                  </div>
-                  <PrimaryButton
-                     // primaryDisable={checkButtonIfDisable()}
-                     onClick={handleCalculateClick}>
-                     Рассчитать
-                  </PrimaryButton>
-               </div>
+    dispatch({ type: t.SET_CURRENT_COMPONENT, payload: t.RESULT_PAGE });
+  }
+
+  return (
+    <CalculateFormStyled>
+      <div>
+        <div className="calculate_form_wrapper">
+          <div className="calculate_form_column">
+            <div>
+              <span className="input_label">* Ваше имя</span>
+              <InputStyled
+                border
+                placeholder="Петро"
+                value={customerName || ''}
+                onChange={e => dispatch({ type: t.SET_CUSTOMER_NAME, payload: e.target.value })}
+              />
             </div>
-            <span className='necessary_field'>* - Обязательное к заполнению поле</span>
-         </div>
-      </CalculateFormStyled>
-   );
+            <div>
+              <span className="input_label">Название организации</span>
+              <InputStyled
+                border
+                placeholder="ТОВ"
+                value={customerOrganization || ''}
+                onChange={e => dispatch({ type: t.SET_CUSTOMER_ORGANIZATION, payload: e.target.value })}
+              />
+            </div>
+            <div>
+              <span className="input_label">* Номер телефона</span>
+              <PhoneInput
+                value={customerPhone || ''}
+                onChange={value => dispatch({ type: t.SET_CUSTOMER_PHONE, payload: value })}
+              />
+            </div>
+          </div>
+          <div className="calculate_form_column">
+            <div>
+              <span className="input_label">* Форма оплаты</span>
+              <Dropdown
+                selectedItem={customerPaymentMethod}
+                callback={(title, id, alias) =>
+                  dispatch({ type: t.SET_CUSTOMER_PAYMENT_METHOD, payload: title, alias })
+                }
+                withBorder={true}
+                list={paymentMethodList}
+              />
+            </div>
+            <div>
+              <span className="input_label">* Адрес</span>
+              <InputPlaces address={addr} dispatch={dispatch} />
+            </div>
+            <PrimaryButton
+              loading={isLoading}
+              primaryDisable={
+                //   !customerName ||
+                //   customerPhone.length !== 19 ||
+                //   !customerPaymentMethod ||
+                !selectedCoordinates || !addr
+
+                //   !materialWeight
+              }
+              onClick={handleCalculateClick}>
+              Рассчитать
+            </PrimaryButton>
+          </div>
+        </div>
+        <span className="necessary_field">* - Обязательное к заполнению поле</span>
+      </div>
+    </CalculateFormStyled>
+  );
 };
 
-const mapStateToProps = ({ firstPage }) => ({
-   materialWeight: firstPage.materialWeight,
-   materialVolume: firstPage.materialVolume,
-   
+const mapStateToProps = ({ firstPage, globalData }) => ({
+  weight: firstPage.materialWeight,
+  paymentMethodList: globalData.paymentMethodList,
+  materialTypeTitle: firstPage.materialTypeTitle,
+  paymentMethodAlias: firstPage.paymentMethodAlias,
+  selectedCoordinates: globalData.selectedCoordinates,
+  phone: firstPage.customerPhone,
 });
 
 export default connect(mapStateToProps)(CalculateForm);
--- src/components/desktop/main-content/main-section/white-section/map.jsx
+++ src/components/desktop/main-content/main-section/white-section/map.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import styled from 'styled-components';
+import L from 'leaflet';
+import { connect } from 'react-redux';
+import * as t from '../../../../../redux/actionTypes';
+
+import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+
+const googleKey = 'AIzaSyCCp4PAFzaCMIY47nyPLZFMfPTlsEp_NUY';
+const mapBoxToken = 'pk.eyJ1Ijoicm9tYXVhIiwiYSI6ImNpbXE3MXcwazAwMWF2cG0xanI0cTFvaTYifQ.zbbHBc9O2okw2n6fmuRFsg';
+const attributionString = 'Map data &copy; OpenStreetMap, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
 const MapStyed = styled.div`
-  /* background-color: lightskyblue; */
   width: 100%;
   height: 100%;
   display: grid;
@@ -10,22 +18,132 @@ const MapStyed = styled.div`
     display: grid;
     grid-template-rows: 30px 1fr;
     margin: 30px 0;
-    /* background-color: lightgreen; */
-
-    .map {
-      /* background-color: lightpink; */
+    span {
+      color: ${({ theme }) => theme.textGrey};
     }
   }
 `;
+// const MapComponentStyled = styled.div`
+//   height: 100%;
+//   .map {
+//     height: 100%;
+//   }
+// `;
 
-const MapSection = () => {
+// function MapComponent({ selectedCoordinates }) {
+//   const markerLat = selectedCoordinates.lat;
+//   const markerLng = selectedCoordinates.lng;
+
+//   const mapRef = useRef(null);
+
+//   function onMapClick(e) {
+//     // setMarkerLat(e.latlng.lat);
+//     // setMarkerLng(e.latlng.lng);
+//     console.log(e.latlng.lat, e.latlng.lng);
+//   }
+
+//   useEffect(() => {
+//     mapRef.current = L.map('mapid').setView([50.44941, 30.524184], 11);
+
+//     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//       attribution: attributionString,
+//       maxZoom: 18,
+//       id: 'mapbox/streets-v11',
+//       tileSize: 512,
+//       zoomOffset: -1,
+//       accessToken: mapBoxToken,
+//     }).addTo(mapRef.current);
+
+//     mapRef.current.on('click', onMapClick);
+//   }, []);
+
+//   // add marker
+//   const markerRef = useRef(null);
+//   useEffect(() => {
+//     if (markerRef.current) {
+//       markerRef.current.setLatLng([markerLat, markerLng]);
+//     } else {
+//       markerRef.current = L.marker([markerLat, markerLng]).addTo(mapRef.current);
+//     }
+//   }, [markerLat, markerLng]);
+
+//   return (
+//     <MapComponentStyled>
+//       <div className="map" id="mapid" />
+//     </MapComponentStyled>
+//   );
+// }
+
+// function MapContainer() {
+//   return (
+//     <MapStyed>
+//       <div className="map_wrapper">
+//         <span>Вы можете ввести адрес, поставив точку на карте</span>
+//         <MapComponent />
+//       </div>
+//     </MapStyed>
+//   );
+// }
+
+function Map({ selectedCoordinates, dispatch }) {
+  const defaultCenterCoords = { lat: 50.44941, lng: 30.524184 };
+
+  function handleOnMapClick(e) {
+    let latlng = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    dispatch({ type: t.SET_COORDINATES, payload: latlng });
+
+    let geocoder = new window.google.maps.Geocoder();
+
+    geocoder.geocode({ location: latlng }, function (results, status) {
+      if (status == 'OK') {
+        dispatch({ type: t.SET_CUSTOMER_ADDRESS, payload: results[0].formatted_address });
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  const map = useRef();
+
+  useEffect(() => {
+    if (selectedCoordinates) {
+      map.current.panTo({ lat: selectedCoordinates.lat, lng: selectedCoordinates.lng });
+    }
+  }, [selectedCoordinates]);
+
+  return (
+    <GoogleMap
+      ref={map}
+      defaultZoom={10}
+      defaultOptions={{
+        mapTypeControl: false,
+        fullscreenControl: false,
+        streetViewControl: false,
+        gestureHandling: 'greedy',
+      }}
+      onClick={handleOnMapClick}
+      defaultCenter={defaultCenterCoords}>
+      <Marker position={selectedCoordinates} />
+    </GoogleMap>
+  );
+}
+
+export default function GoogleMapsComponent() {
   return (
     <MapStyed>
       <div className="map_wrapper">
         <span>Вы можете ввести адрес, поставив точку на карте</span>
-        <div className="map">map</div>
+        <MapWrapped
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${googleKey}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}></MapWrapped>
       </div>
     </MapStyed>
   );
-};
-export default MapSection;
+}
+
+const mapStateToProps = ({ globalData }) => ({
+  selectedCoordinates: globalData.selectedCoordinates,
+});
+const MapWrapped = withGoogleMap(connect(mapStateToProps)(Map));
