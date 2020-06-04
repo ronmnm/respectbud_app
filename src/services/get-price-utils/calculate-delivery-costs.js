a859ec9005f65e5f75b7c9a79175a8e3f53ba1c6
ronmnm <spellbound.2808@gmail.com>
2020-06-04 19:00:47 +0300

    delivery costs test

--- src/services/get-price-utils/calculate-delivery-costs.js
+++ src/services/get-price-utils/calculate-delivery-costs.js
@@ -1,4 +1,3 @@
-
 /**
  * Function returns delivery costs for particular truck depending on weight
  * @param distances is array of distances, each elements is also array
@@ -6,12 +5,14 @@
  */
 export function calculateDeliveryCost(distances, weight) {
   if (weight <= 7) {
-    return _getCostFor_7_10_20(distances, 35, 500, "Зил")
+    return _getCostFor7(distances, "Зил")
+    // return _getCostFor_7_10_20(distances, 35, 500, "Зил")
   } else if (8 <= weight && weight <= 10) {
     return _getCostFor_7_10_20(distances, 35, 600, "Камаз")
   } else if (11 <= weight && weight <= 20) {
-    // eсли вес >= 20 то + карьеры по песку 
-    return _getCostFor_7_10_20(distances, 55, 700, "Маз")
+    // eсли вес >= 20 то + карьеры по песку
+    return _getCostFor20(distances, "Маз")
+    // return _getCostFor_7_10_20(distances, 55, 700, "Маз")
   } else if (21 <= weight && weight <= 30) {
     // eсли вес >= 30 то + карьеры по щебню - 200 маржа
     return _getCostFor30(distances, "30 Tонник")
@@ -33,6 +34,101 @@ export function calculateDeliveryCost(distances, weight) {
       return { distance, initialDistance: kmArray[0], deliveryPrice, truck }
     })
   }
+
+  function _getCostFor7(distances, truck) {
+    return distances.map(kmArray => {
+      let distance = _adjustDistance(Math.round(kmArray[0]))
+      let deliveryPrice
+      if (1 <= distance && distance <= 5) {
+        deliveryPrice = 600
+      } else if (6 <= distance && distance <= 10) {
+        deliveryPrice = 800
+      } else if (11 <= distance && distance <= 15) {
+        deliveryPrice = distance * 78
+      } else if (16 <= distance && distance <= 20) {
+        deliveryPrice = distance * 60
+      } else if (distance >= 21) {
+        deliveryPrice = distance * 58
+      }
+
+      return { distance, initialDistance: kmArray[0], deliveryPrice, truck }
+    })
+  }
+
+  function _getCostFor20(distances, truck) {
+    let mazPricesArray = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      1059,
+      1140,
+      1228,
+      1311,
+      1400,
+      1495,
+      1571,
+      1666,
+      1748,
+      1824,
+      1907,
+      1976,
+      2060,
+      2141,
+      2223,
+      2305,
+      2381,
+      2457,
+      2527,
+      2609,
+      2685,
+      2768,
+      2831,
+      2920,
+      3000,
+      3085,
+      3167,
+      3237,
+      3319,
+      3395,
+      3477,
+      3559,
+      3630,
+      3700,
+      3775,
+      3850,
+      3900,
+      3980,
+      4050,
+      4117,
+    ]
+
+    return distances.map(kmArray => {
+      let distance = _adjustDistance(Math.round(kmArray[0]))
+      let deliveryPrice
+
+      window.mazPricesArray = mazPricesArray
+      if (1 <= distance && distance <= 5) {
+        deliveryPrice = 700
+      } else if (5 <= distance && distance <= 10) {
+        deliveryPrice = 900
+      } else if (11 <= distance && distance <= 50) {
+        deliveryPrice = mazPricesArray[distance]
+      } else if (distance >= 51) {
+        deliveryPrice = 5000
+      }
+
+      return { distance, initialDistance: kmArray[0], deliveryPrice, truck }
+    })
+  }
+
   function _getCostFor30(distances, truck) {
     return distances.map(kmArray => {
       let distance = _adjustDistance(Math.round(kmArray[0]))
